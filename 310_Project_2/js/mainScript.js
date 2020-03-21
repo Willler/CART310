86427ae572d366f4b5efe99ff7808e0fa902cc3b
William L'Eriger <espermentalout@gmail.com>
2020-03-20 23:56:28 -0400

    P2: Added almost all other functionality and handling

--- 310_Project_2/js/mainScript.js
+++ 310_Project_2/js/mainScript.js
@@ -12,9 +12,32 @@ let greenUserEntranceX;
 let greenVelocityX = 0;
 let greenSpeed = 2;
 
-let blueUserEntranceX;
-let blueVelocityX = 0;
+let blueUserEntranceY;
+let blueVelocityY = 0;
 let blueSpeed = 2;
+
+//variables for display screen
+let redUX;
+let redUXVelocity = 0;
+let redUY;
+let redUYVelocity = 0;
+let redUXSpeed = 2;
+let redUYSpeed = 2;
+
+let greenUX;
+let greenUXVelocity = 0;
+let greenUY;
+let greenUYVelocity = 0;
+let greenUXSpeed = 2;
+let greenUYSpeed = 2;
+
+let blueUX;
+let blueUXVelocity = 0;
+let blueUY;
+let blueUYVelocity = 0;
+let blueUXSpeed = 2;
+let blueUYSpeed = 2;
+
 
 function setup() {
 
@@ -29,6 +52,7 @@ function setup() {
   // mapOutline();
 
   setupEntrance();
+  setupDisplay();
 }
 
 
@@ -39,6 +63,7 @@ function draw() {
   moveStuff();
   redUserEntrance();
   greenUserEntrance();
+  blueUserEntrance();
 }
 
 function mapOutline() {
@@ -65,10 +90,12 @@ function mapOutline() {
   rect((windowWidth/2 + 300)/2, 650, 25, 45);
   rect((windowWidth/2 + 300)/2, 750, 25, 45);
 
-  //entrances
+  ///////////////////////////entrances
   //red
   stroke(255, 0 , 0);
   rect(windowWidth/2 + 530, (windowHeight/3)/2, 420, 250, 5);
+  textSize(24);
+  text('Hold Z', windowWidth/2 + 350, (windowHeight/3)/2 - 75);
   stroke(255);
   //narrow
   line(windowWidth/2 + 320, 125, windowWidth/2 + 530, 125);
@@ -86,6 +113,7 @@ function mapOutline() {
   //green
   stroke(0, 255, 0);
   rect(windowWidth/2 + 530, windowHeight/2, 420, 250, 5);
+  text('Hold X', windowWidth/2 + 350, windowHeight/2 - 75);
   stroke(255);
   //narrow
   line(windowWidth/2 + 320, 415, windowWidth/2 + 530, 415);
@@ -96,7 +124,7 @@ function mapOutline() {
   //connecting
   line(windowWidth/2 + 530, 360, windowWidth/2 + 530, 415);
   line(windowWidth/2 + 530, 520, windowWidth/2 + 530, 465);
-  //
+  // stairs/progress
   line(windowWidth/2 + 500, 425, windowWidth/2 + 500, 455);
   line(windowWidth/2 + 470, 425, windowWidth/2 + 470, 455);
   line(windowWidth/2 + 440, 425, windowWidth/2 + 440, 455);
@@ -106,6 +134,22 @@ function mapOutline() {
   //blue
   stroke(0, 0, 255);
   rect(windowWidth/2 + 530, ((windowHeight/3)*2.49), 420, 250, 5);
+  text('Hold C', windowWidth/2 + 350, ((windowHeight/3)*2.49) - 75);
+  stroke(255);
+  //outlines
+  line(windowWidth/2 + 480, 593, windowWidth/2 + 480, 670);
+  line(windowWidth/2 + 580, 593, windowWidth/2 + 580, 670);
+  line(windowWidth/2 + 400, 720, windowWidth/2 + 480, 670);
+  line(windowWidth/2 + 660, 720, windowWidth/2 + 580, 670);
+  line(windowWidth/2 + 400, 720, windowWidth/2 + 400, 842);
+  line(windowWidth/2 + 660, 720, windowWidth/2 + 660, 842);
+  //stairs
+  line(windowWidth/2 + 430, 740, windowWidth/2 + 630, 740);
+  line(windowWidth/2 + 430, 760, windowWidth/2 + 630, 760);
+  line(windowWidth/2 + 430, 780, windowWidth/2 + 630, 780);
+  //trigger
+  stroke(0,0,255);
+  line(windowWidth/2 + 430, 820, windowWidth/2 + 630, 820);
 
   //shop lines
   stroke(255);
@@ -134,61 +178,152 @@ function redUserEntrance() {
   noStroke();
   fill(255, 0, 0);
   ellipse(redUserEntranceX, (windowHeight/2)/3 + 6, 25, 25);
+
+  if (redUserEntranceX > windowWidth/2 + 710) {
+    redUserDisplay();
+  }
+}
+
+function redUserDisplay() {
+  ellipseMode(CENTER);
+  noStroke();
+  fill(255,0,0);
+  ellipse (redUX, redUY, 25, 25);
 }
 
 function greenUserEntrance() {
   noStroke();
   fill(0, 255, 0);
   ellipse(greenUserEntranceX, windowHeight/2 + 6, 25, 25);
+
+  if (greenUserEntranceX < windowWidth/2 + 360) {
+    greenUserDisplay();
+  }
+}
+
+function greenUserDisplay() {
+  ellipseMode(CENTER);
+  noStroke();
+  fill(0,255,0);
+  ellipse (greenUX, greenUY, 25, 25);
+}
+
+function blueUserEntrance() {
+  noStroke();
+  fill(0,0,255);
+  ellipse((windowWidth/2 + 530), blueUserEntranceY, 25, 25);
+
+  if (blueUserEntranceY > 820) {
+    blueUserDisplay();
+  }
+}
+
+function blueUserDisplay() {
+  ellipseMode(CENTER);
+  noStroke();
+  fill(0,0,255);
+  ellipse (blueUX, blueUY, 25, 25);
 }
 
 function setupEntrance() {
   redUserEntranceX = (windowWidth/2 + 350);
   greenUserEntranceX = (windowWidth/2 + 700);
+  blueUserEntranceY = ((windowHeight/3)*2.49 - 80);
+}
+
+function setupDisplay() {
+  redUX = 50;
+  redUY = 630;
+  greenUX = 1000;
+  greenUY = 267;
+  blueUX = 330;
+  blueUY = 50;
 }
 
 function handleInput() {
-  //hold p
+  //hold z
   if (keyIsDown(90)) {
     redVelocityX = redSpeed;
   } else {
     redVelocityX = 0;
   }
-
+//x
   if (keyIsDown(88)) {
     greenVelocityX = -greenSpeed;
   } else {
     greenVelocityX = 0;
   }
+//c
+  if (keyIsDown(67)) {
+    blueVelocityY = blueSpeed;
+  } else {
+    blueVelocityY = 0;
+  }
+//ws
+  if (keyIsDown(87)) {
+    redUYVelocity = -redUYSpeed;
+  } else if (keyIsDown(83)) {
+    redUYVelocity = redUYSpeed;
+  } else {
+    redUYVelocity = 0;
+  }
+  //ad
+  if (keyIsDown(65)) {
+    redUXVelocity = -redUXSpeed;
+  } else if (keyIsDown(68)) {
+    redUXVelocity = redUXSpeed;
+  } else {
+    redUXVelocity = 0;
+  }
+
+  //updown
+    if (keyIsDown(UP_ARROW)) {
+      greenUYVelocity = -greenUYSpeed;
+    } else if (keyIsDown(DOWN_ARROW)) {
+      greenUYVelocity = greenUYSpeed;
+    } else {
+      greenUYVelocity = 0;
+    }
+
+    //LEFTRIGHT
+      if (keyIsDown(LEFT_ARROW)) {
+        greenUXVelocity = -greenUXSpeed;
+      } else if (keyIsDown(RIGHT_ARROW)) {
+        greenUXVelocity = greenUXSpeed;
+      } else {
+        greenUXVelocity = 0;
+      }
+
+      //uj
+        if (keyIsDown(85)) {
+          blueUYVelocity = -blueUYSpeed;
+        } else if (keyIsDown(74)) {
+          blueUYVelocity = blueUYSpeed;
+        } else {
+          blueUYVelocity = 0;
+        }
+
+        //hk
+          if (keyIsDown(72)) {
+            blueUXVelocity = -blueUXSpeed;
+          } else if (keyIsDown(75)) {
+            blueUXVelocity = blueUXSpeed;
+          } else {
+            blueUXVelocity = 0;
+          }
 }
 
 function moveStuff() {
   redUserEntranceX += redVelocityX;
   greenUserEntranceX += greenVelocityX;
-}
+  blueUserEntranceY += blueVelocityY;
 
-
-
-
-
-
-
-
-
-function blueUserEntrance() {
-
-}
-
-function redUser() {
-
-}
-
-function greenUser() {
-
-}
-
-function blueUser() {
-
+  redUX += redUXVelocity;
+  redUY += redUYVelocity;
+  greenUX += greenUXVelocity;
+  greenUY += greenUYVelocity;
+  blueUX += blueUXVelocity;
+  blueUY += blueUYVelocity;
 }
 
 function crowdSimulation() {
